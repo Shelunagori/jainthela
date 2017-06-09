1322e9a426e6f47c906e2045abcda82cc2f525a1
Abhilash Lohar <abhilashlohar01@gmail.com>
2017-06-09 17:49:17 +0530

    asdas

--- webroot/jainthela (2).sql
+++ webroot/jainthela (2).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 09, 2017 at 12:29 PM
+-- Generation Time: Jun 09, 2017 at 02:17 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -43,6 +43,18 @@ INSERT INTO `cities` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `companies`
+--
+
+CREATE TABLE `companies` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `franchise_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `franchises`
 --
 
@@ -60,7 +72,7 @@ CREATE TABLE `franchises` (
 
 CREATE TABLE `franchise_item_categories` (
   `franchise_id` int(11) NOT NULL,
-  `city_id` int(11) NOT NULL
+  `item_category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -86,6 +98,25 @@ CREATE TABLE `item_categories` (
   `id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `data` blob NOT NULL,
+  `expires` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `data`, `expires`) VALUES
+('p88f2sev4h2d0kk64bv5teg7d2', 0x436f6e6669677c613a313a7b733a343a2274696d65223b693a313439373031303232383b7d466c6173687c613a303a7b7d417574687c613a313a7b733a343a2255736572223b613a363a7b733a323a226964223b693a313b733a383a22757365726e616d65223b733a363a22617368697368223b733a343a22726f6c65223b733a313a2231223b733a363a22737461747573223b733a313a2231223b733a373a2263726561746564223b4f3a32303a2243616b655c4931386e5c46726f7a656e54696d65223a333a7b733a343a2264617465223b733a32363a22323031372d30362d30392030313a30303a30302e303030303030223b733a31333a2274696d657a6f6e655f74797065223b693a333b733a383a2274696d657a6f6e65223b733a333a22555443223b7d733a383a226d6f646966696564223b4e3b7d7d, -650473417);
 
 -- --------------------------------------------------------
 
@@ -116,6 +147,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `status`, `created`, `modified`) VALUES
+(1, 'ashish', '$2y$10$rg61cBlo66vJL.MdAGhokuGiyDaLF9BN34gyy38..aPZRvl39.jSG', '1', '1', '2017-06-09 01:00:00', NULL);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -123,6 +161,12 @@ CREATE TABLE `users` (
 -- Indexes for table `cities`
 --
 ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -135,8 +179,8 @@ ALTER TABLE `franchises`
 -- Indexes for table `franchise_item_categories`
 --
 ALTER TABLE `franchise_item_categories`
-  ADD PRIMARY KEY (`franchise_id`,`city_id`),
-  ADD KEY `company_key` (`city_id`);
+  ADD PRIMARY KEY (`franchise_id`,`item_category_id`),
+  ADD KEY `company_key` (`item_category_id`);
 
 --
 -- Indexes for table `items`
@@ -148,6 +192,12 @@ ALTER TABLE `items`
 -- Indexes for table `item_categories`
 --
 ALTER TABLE `item_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -173,6 +223,11 @@ ALTER TABLE `users`
 ALTER TABLE `cities`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `franchises`
 --
 ALTER TABLE `franchises`
@@ -196,7 +251,7 @@ ALTER TABLE `units`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
